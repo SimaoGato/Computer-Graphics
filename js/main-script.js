86564e3efd93fccef4d4f849f90edc4cda4bc18f
Simão Gato <simao.valedegato@gmail.com>
2023-05-16 18:48:39 +0100

    Add functions to create 4 cameras: frontal, lateral, upper and perspective

--- js/main-script.js
+++ js/main-script.js
@@ -2,6 +2,8 @@
 /* GLOBAL VARIABLES */
 //////////////////////
 
+/* cameras vars */ var frontCamera, upperCamera, lateralCamera, perspectiveCamera;
+
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -14,7 +16,59 @@ function createScene(){
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
+function createPerspectiveCamera() {
+    'use strict';
+    perspectiveCamera = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    perspectiveCamera.position.x = 50;
+    perspectiveCamera.position.y = 50;
+    perspectiveCamera.position.z = 50;
+    perspectiveCamera.lookAt(scene.position);
+}
 
+function createFrontCamera() {
+    'use strict';
+    frontCamera = new THREE.OrthographicCamera(window.innerWidth / -13,
+                                         window.innerWidth / 13,
+                                         window.innerHeight / 13,
+                                         window.innerHeight / -13,
+                                         0.1,
+                                         1000);
+    frontCamera.position.x = 50;
+    frontCamera.position.y = 0;
+    frontCamera.position.z = 0;
+    frontCamera.lookAt(scene.position);
+}
+
+function createUpperCamera() {
+    'use strict';
+    upperCamera = new THREE.OrthographicCamera(window.innerWidth / -13,
+                                         window.innerWidth / 13,
+                                         window.innerHeight / 13,
+                                         window.innerHeight / -13,
+                                         0.1,
+                                         1000);
+    upperCamera.position.x = 0;
+    upperCamera.position.y = 50;
+    upperCamera.position.z = 0;
+    upperCamera.lookAt(scene.position);
+}
+
+function createLateralCamera() {
+    'use strict';
+    lateralCamera = new THREE.OrthographicCamera(window.innerWidth / -13,
+                                         window.innerWidth / 13,
+                                         window.innerHeight / 13,
+                                         window.innerHeight / -13,
+                                         0.1,
+                                         1000);
+    lateralCamera.position.x = 0;
+    lateralCamera.position.y = 0;
+    lateralCamera.position.z = 50;
+    lateralCamera.lookAt(scene.position);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
