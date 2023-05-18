b8ce1ea79c32e78671f0b6cdd7f855a6b6820a35
rodrigo-sl <rodrigo.s.lopes@tecnico.ulisboa.pt>
2023-05-18 16:23:23 +0100

    Robot: added waist, abdomen and torso. Added background color

--- js/main-script.js
+++ js/main-script.js
@@ -6,6 +6,8 @@ var scene, renderer;
 
 /* cameras vars */ var camera, frontCamera, upperCamera, lateralCamera, perspectiveCamera;
 
+/* Robot */ var robot, robotMaterial;
+
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
@@ -14,8 +16,10 @@ function createScene(){
 
     scene = new THREE.Scene();
 
+    scene.background = new THREE.Color(0x404040);
     scene.add(new THREE.AxisHelper(10));
 
+    createRobot();
 }
 
 //////////////////////
@@ -27,9 +31,9 @@ function createPerspectiveCamera() {
                                          window.innerWidth / window.innerHeight,
                                          1,
                                          1000);
-    perspectiveCamera.position.x = 50;
-    perspectiveCamera.position.y = 50;
-    perspectiveCamera.position.z = 50;
+    perspectiveCamera.position.x = 20;
+    perspectiveCamera.position.y = 20;
+    perspectiveCamera.position.z = 20;
     perspectiveCamera.lookAt(scene.position);
 }
 
@@ -41,9 +45,9 @@ function createFrontCamera() {
                                          window.innerHeight / -13,
                                          0.1,
                                          1000);
-    frontCamera.position.x = 50;
+    frontCamera.position.x = 0;
     frontCamera.position.y = 0;
-    frontCamera.position.z = 0;
+    frontCamera.position.z = 10;
     frontCamera.lookAt(scene.position);
 }
 
@@ -56,7 +60,7 @@ function createUpperCamera() {
                                          0.1,
                                          1000);
     upperCamera.position.x = 0;
-    upperCamera.position.y = 50;
+    upperCamera.position.y = 10;
     upperCamera.position.z = 0;
     upperCamera.lookAt(scene.position);
 }
@@ -69,9 +73,9 @@ function createLateralCamera() {
                                          window.innerHeight / -13,
                                          0.1,
                                          1000);
-    lateralCamera.position.x = 0;
+    lateralCamera.position.x = 10;
     lateralCamera.position.y = 0;
-    lateralCamera.position.z = 50;
+    lateralCamera.position.z = 0;
     lateralCamera.lookAt(scene.position);
 }
 
@@ -82,6 +86,82 @@ function createLateralCamera() {
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+function createRobot(){
+    'use strict';
+
+    var gWaist;
+
+    robotMaterial = new THREE.MeshBasicMaterial({color: 0x0000ff,wireframe: false });
+
+    robot = new THREE.Object3D();
+    robot.position.set(0, 0, 0);
+
+    createRobotWaist(gWaist);
+    //createRobotAbdomen();
+    //createRobotTorso();
+
+    scene.add(robot);
+}
+
+function createRobotWaist(gWaist){
+
+    'use strict';
+
+    gWaist = new THREE.Object3D();
+
+    var xWaist = 10, yWaist = 4, zWaist = 10;
+    var pWaist = new THREE.Mesh(new THREE.BoxGeometry(xWaist, yWaist, zWaist), robotMaterial)
+    pWaist.position.set(0, 0, 0);
+    gWaist.add(pWaist);
+
+    var radTopWheel = 2, radBottomWheel = 2, heightWheel = 2;
+    var pRightWheel = new THREE.Mesh(new THREE.CylinderGeometry(radTopWheel, radBottomWheel, heightWheel), robotMaterial);
+    pRightWheel.rotation.z = Math.PI / 2;
+    pRightWheel.position.set(- (xWaist / 2 + heightWheel / 2), - yWaist / 4, 0);
+    gWaist.add(pRightWheel);
+
+    var pLeftWheel = new THREE.Mesh(new THREE.CylinderGeometry(radTopWheel, radBottomWheel, heightWheel), robotMaterial);
+    pLeftWheel.rotation.z = Math.PI / 2;
+    pLeftWheel.position.set(xWaist / 2 + heightWheel / 2,  - yWaist / 4, 0);
+    gWaist.add(pLeftWheel);
+
+    createRobotAbdomen(gWaist, yWaist);
+
+    robot.add(gWaist);
+}
+
+function createRobotAbdomen(gWaist, yWaist){
+
+    'use strict';
+
+    var gAbdomen = new THREE.Object3D();
+    var xAbdomen = 8, yAbdomen = 3, zAbdomen = 10;
+    gAbdomen.position.set(0, yWaist / 2 + yAbdomen / 2, 0);
+
+    var pAbdomen = new THREE.Mesh(new THREE.BoxGeometry(xAbdomen, yAbdomen, zAbdomen), robotMaterial);
+    pAbdomen.position.set(0, 0, 0);
+    gAbdomen.add(pAbdomen);
+    
+    createRobotTorso(gAbdomen, yAbdomen);
+
+    gWaist.add(gAbdomen);
+}
+
+function createRobotTorso(gAbdomen, yAbdomen){
+
+    'use strict';
+
+    var gTorso = new THREE.Object3D();
+    var xTorso = 14, yTorso = 6, zTorso = 10;
+    gTorso.position.set(0, yAbdomen / 2 + yTorso / 2, 0);
+
+    var pTorso = new THREE.Mesh(new THREE.BoxGeometry(xTorso, yTorso, zTorso), robotMaterial);
+    pTorso.position.set(0, 0, 0);
+    gTorso.add(pTorso);
+
+    gAbdomen.add(gTorso);
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -129,7 +209,7 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     createScene();
-    
+
     createPerspectiveCamera();
     createFrontCamera();
     createUpperCamera();
@@ -187,6 +267,15 @@ function onKeyDown(e) {
             break;
         case 52: // 4 - Perspective Camera
             camera = perspectiveCamera;
+            break;
+
+        case 54: // 6 - Wireframe
+    
+            scene.traverse(function (node) {
+                if (node instanceof THREE.Mesh) {
+                    node.material.wireframe = !node.material.wireframe;
+                }
+            });
             break;
     }
 }
